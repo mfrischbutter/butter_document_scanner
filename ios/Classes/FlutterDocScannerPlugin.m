1dab24b37f5d70e9daa72f54ca23b414177f8c33
Michael Frischbutter <michael.frischbutter@zep.de>
2025-05-27 14:07:26 +0200

    fix: ios build

--- ios/Classes/FlutterDocScannerPlugin.m
+++ ios/Classes/FlutterDocScannerPlugin.m
@@ -1,11 +1,11 @@
 #import "FlutterDocScannerPlugin.h"
-#if __has_include(<flutter_doc_scanner/flutter_doc_scanner-Swift.h>)
-#import <flutter_doc_scanner/flutter_doc_scanner-Swift.h>
+#if __has_include(<butter_document_scanner/butter_document_scanner-Swift.h>)
+#import <butter_document_scanner/butter_document_scanner-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_doc_scanner-Swift.h"
+#import "butter_document_scanner-Swift.h"
 #endif
 
 @implementation FlutterDocScannerPlugin
